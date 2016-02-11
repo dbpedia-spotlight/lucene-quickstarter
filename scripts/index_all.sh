356e6e95e04ace587d0b999f32f9b6387c1cf479
Sandro Athaide Coelho <sandroacoelho@gmail.com>
2016-02-11 20:38:41 -0200

    Adding script, config files etc

---
+++ scripts/index_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+readonly DBPEDIA_VERSION=$1
+./index_config_file.sh ${DBPEDIA_VERSION} ar arabic org.apache.lucene.analysis.ar.ArabicAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} cz czech  org.apache.lucene.analysis.cz.CzechAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} de german  org.apache.lucene.analysis.de.GermanAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} el greek  org.apache.lucene.analysis.el.GreekAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} en english  org.apache.lucene.analysis.en.EnglishAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} es spanish  org.apache.lucene.analysis.es.SpanishAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} fr french  org.apache.lucene.analysis.fr.FrenchAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} it italian  org.apache.lucene.analysis.it.ItalianAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} ko korean  org.apache.lucene.analysis.cz.CzechAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} nl dutch  org.apache.lucene.analysis.nl.DutchAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} pl polish  org.apache.lucene.analysis.pl.PolishAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} pt portuguese  org.apache.lucene.analysis.pt.PortugueseAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} hu hungarian  org.apache.lucene.analysis.hu.HungarianAnalyzer
+./index_config_file.sh ${DBPEDIA_VERSION} ru russian org.apache.lucene.analysis.ru.RussianAnalyzer
---
+++ scripts/index_config_file.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+readonly ROOT_DIRECTORY=/mnt/dbpedia
+readonly DBPEDIA_VERSION=$1
+readonly LANG_i18n=$2
+readonly LANG_DESC=$3
+readonly LUCENE_CLASS=$4
+
+echo "# Wikipedia Dump" > ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# --------------" >> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.wikipediaDump=${ROOT_DIRECTORY}/wikipedia/${LANG_i18n}/${LANG_i18n}wiki-latest-pages-articles.xml.bz2">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# Location for DBpedia resources index (output)">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.index.dir=${ROOT_DIRECTORY}/spotlight/${LANG_i18n}/index">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.index.minDocsBeforeFlush=40000">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# DBpedia Datasets">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# ----------------">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.labels=${ROOT_DIRECTORY}/dbpedia_data/3.9/${LANG_i18n}/labels_${LANG_i18n}.nt.bz2">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.redirects=${ROOT_DIRECTORY}/dbpedia_data/3.9/${LANG_i18n}/redirects_${LANG_i18n}.nt.bz2">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.disambiguations=${ROOT_DIRECTORY}/dbpedia_data/3.9/${LANG_i18n}/disambiguations_${LANG_i18n}.nt.bz2">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.instanceTypes=${ROOT_DIRECTORY}/dbpedia_data/3.9/${LANG_i18n}/instance_types_${LANG_i18n}.nt.bz2">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# Files created from DBpedia Datasets">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# -----------------------">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.conceptURIs=${ROOT_DIRECTORY}/spotlight/${LANG_i18n}/conceptURIs.list">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.redirectsTC=${ROOT_DIRECTORY}/spotlight/${LANG_i18n}/redirects_tc.tsv">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.surfaceForms=${ROOT_DIRECTORY}/spotlight/${LANG_i18n}/surfaceForms-fromOccs.tsv">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# Language-specific config">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# --------------">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.language=${LANG_DESC}">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.language_i18n_code=cs">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.lucene.analyzer=${LUCENE_CLASS}">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.lucene.version=LUCENE_36">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# Internationalization (i18n) support">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.default_namespace=http://${LANG_i18n}.dbpedia.org/resource/">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.default_ontology=http://dbpedia.org/ontology/">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# Stop word list">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.stopWords.${LANG_DESC}=${ROOT_DIRECTORY}/lucene-quickstarter/i18n/${LANG_i18n}/stopwords.list">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# URI patterns that should not be indexed. e.g. List_of_*">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.badURIs.${LANG_DESC}=${ROOT_DIRECTORY}/lucene-quickstarter/i18n/${LANG_i18n}/blacklistedURIPatterns.${LANG_i18n}.list">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# Will discard surface forms that are too long (reduces complexity of spotting and generally size in disk/memory)">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.maxSurfaceFormLength=50">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "# Will index only words closest to resource occurrence">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.maxContextWindowSize=200">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
+echo "org.dbpedia.spotlight.data.minContextWindowSize=0">> ../i18n/${LANG_i18n}/indexing_${DBPEDIA_VERSION}.properties
