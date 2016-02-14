a65adfe9ced659113fd0df1949595646a2035144
Sandro <sandroacoelho@gmail.com>
2016-02-14 20:05:13 -0200

    Update package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,7 +4,7 @@ VERSION=$1
 LANGUAGE=$2
 
 
-readonly ROOT_DIR=/mnt/dbpedia/
+readonly ROOT_DIR=/mnt/dbpedia
 
 cp ${ROOT_DIR}/lucene-quickstarter/conf/similarity-thresholds.txt ${ROOT_DIR}/spotlight/${LANGUAGE}/index-withSF-withTypes
-tar -zcvf ${LANGUAGE}_${VERSION} ${ROOT_DIR}/spotlight/${LANGUAGE}/index-withSF-withTypes ${ROOT_DIR}/lucene-quickstarter/conf ${ROOT_DIR}/lucene-quickstarter/i18n/${LANGUAGE}.tar.gz
+tar -zcvf ${LANGUAGE}_${VERSION}.tar.gz ${ROOT_DIR}/spotlight/${LANGUAGE}/index-withSF-withTypes ${ROOT_DIR}/lucene-quickstarter/conf ${ROOT_DIR}/lucene-quickstarter/i18n/${LANGUAGE}
