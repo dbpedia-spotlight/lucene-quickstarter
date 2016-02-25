c092bf451b1ad33ae5adf345edbb56d7a34ee6a1
Sandro <sandroacoelho@gmail.com>
2016-02-25 20:20:04 -0300

    Adding spotter

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,8 +3,7 @@
 VERSION=$1
 LANGUAGE=$2
 
-
 readonly ROOT_DIR=/mnt/dbpedia
 
 cp ${ROOT_DIR}/lucene-quickstarter/conf/similarity-thresholds.txt ${ROOT_DIR}/spotlight/${LANGUAGE}/index-withSF-withTypes
-tar -zcvf ${LANGUAGE}_${VERSION}.tar.gz ${ROOT_DIR}/spotlight/${LANGUAGE}/index-withSF-withTypes ${ROOT_DIR}/lucene-quickstarter/conf ${ROOT_DIR}/lucene-quickstarter/i18n/${LANGUAGE}
+tar -zcvf ${LANGUAGE}_${VERSION}.tar.gz ${ROOT_DIR}/spotlight/${LANGUAGE}/index-withSF-withTypes ${ROOT_DIR}/lucene-quickstarter/conf ${ROOT_DIR}/lucene-quickstarter/i18n/${LANGUAGE} ${ROOT_DIR}/spotlight/${LANGUAGE}/surfaceForms-fromOccs.tsv.spotterDictionary
